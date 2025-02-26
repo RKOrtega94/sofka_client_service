6e6e0b3eb879e9894a0e625496c9685888766bc3
RKOrtega <rkortega1994@gmail.com>
2025-02-25 19:58:04 -0500

    Add ClientModel and ClientEntity classes with properties and relationships

--- src/main/java/com/example/client/modules/client/domain/ClientModel.java
+++ src/main/java/com/example/client/modules/client/domain/ClientModel.java
@@ -1,4 +1,17 @@
 package com.example.client.modules.client.domain;
 
+import com.example.client.modules.person.domain.PersonModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientModel {
+    private Long id;
+    private String clientId;
+    private String password;
+    private boolean status;
+    private PersonModel person;
 }
--- src/main/java/com/example/client/modules/client/infrastructure/persistence/ClientEntity.java
+++ src/main/java/com/example/client/modules/client/infrastructure/persistence/ClientEntity.java
@@ -1,4 +1,29 @@
 package com.example.client.modules.client.infrastructure.persistence;
 
-public class CLientEntity {
+import com.example.client.modules.person.infrastructure.persistence.PersonEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "client_id", unique = true)
+    private String clientId;
+    private String password;
+    private boolean status;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id", referencedColumnName = "client_id")
+    private PersonEntity person;
 }
